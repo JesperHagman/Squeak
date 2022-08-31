1249a3c828fce131874071454c69466e166d9eae
Johan Blomberg <JohanBlomberg89@gmail.com>
2022-08-31 17:19:49 +0200

    us_02 finished first loginmodule

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,6 +1,44 @@
+import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
 const LoginComponent = () => {
-    const 
-    return (  );
+
+const auth = getAuth()
+const [message, setMessage] = useState("")
+const [details, setDetails] = useState({
+    email: "",
+    password: ""
+})
+
+const submitHandler = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, details)
+    .then(userCredentials => {
+        const user = userCredentials.user
+    })
+    .catch(error => setMessage(error.message))
+}
+    
+    return (  
+        <form onSubmit={submitHandler}>
+            <div>
+                <h2>Logga in</h2>
+                    <div className='errorMessage'>
+                        {message}
+                    </div>
+                    <div>
+                        <label htmlFor="email">
+                            <input type="email" name="email" id="email" placeholder="E-mail" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}></input>
+                        </label>                    
+                        <label htmlFor="password">
+                            <input type="password" name="password" id="password" placeholder="Lösenord" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}></input>
+                        </label>                    
+                    </div>
+                    <input type="submit" value="Logga in"></input>
+            </div>
+        </form>
+            
+    );
 }
  
 export default LoginComponent;
